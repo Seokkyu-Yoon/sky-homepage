7660dc0216c4a9a91fe9470c2170972e95487646
윤석규 <yskyu93@modutech.co.kr>
2021-01-26 18:51:25 +0900

    refactor(init): init with electron

---
+++ web/backend/src/app.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import path from 'path'
+import cookieParser from 'cookie-parser'
+import logger from 'morgan'
+import cors from 'cors'
+import ejs from 'ejs'
+
+const app = express()
+
+// view engine setup
+app.set('views', path.join(__dirname, '..', 'public'))
+app.engine('html', ejs.renderFile)
+app.set('view engine', 'ejs')
+
+// CORS
+app.use(cors())
+
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, '..', 'public')))
+
+app.get('/*', (req, res, next) => {
+  res.render('index.html')
+})
+
+export default app
