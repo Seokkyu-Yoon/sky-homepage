42260cdad47f6e44d8cb6a648c59d78b78192cde
윤석규 <yskyu93@modutech.co.kr>
2021-01-27 12:43:39 +0900

    refactor(frontend): refactor views

--- web/frontend/src/router/index.js
+++ web/frontend/src/router/index.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+
 import Home from '../views/Home.vue'
-import About from '../views/About.vue'
+import Storage from '../views/Storage.vue'
 
 Vue.use(VueRouter)
 
@@ -12,9 +13,9 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    component: About
+    path: '/storage',
+    name: 'Storage',
+    component: Storage
   }
 ]
 
---
+++ web/frontend/src/views/Storage.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Storage</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Storage'
+}
+</script>
